86b8bebf33d588f543cd28ff1dc3ef45cf9b69cd
Angelo Silvestre <johnangelo.silvestre04@gmail.com>
2024-09-19 01:12:36 +0800

    feat: add components

---
+++ src/components/layout/MainNav.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+</script>
+
+<template>
+   <nav
+      :class="
+         cn('flex items-center space-x-4 lg:space-x-6', $attrs.class ?? '')
+      "
+   >
+      <a
+         href="/dashboard"
+         class="text-lg font-medium transition-colors hover:text-primary"
+      >
+         Dashboard
+      </a>
+      <a
+         href="/products"
+         class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+         Products
+      </a>
+      <a
+         href="/categories"
+         class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+         Categories
+      </a>
+      <a
+         href="/users"
+         class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+         Users
+      </a>
+   </nav>
+</template>
