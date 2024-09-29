be0f08403c48c3c8aac8da192441ad862916a091
Angelo Silvestre <johnangelo.silvestre04@gmail.com>
2024-09-29 12:35:50 +0800

    refactor: Remove vercel.json and update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,13 +17,4 @@ export default defineConfig({
          '@': path.resolve(__dirname, 'src'),
       },
    },
-   build: {
-      outDir: 'dist', // You can change 'dist' to your preferred output directory
-      sourcemap: false, // Set this to false if you don't want .map files generated
-      rollupOptions: {
-         output: {
-            // Customize the output file structure if needed
-         },
-      },
-   },
 });
